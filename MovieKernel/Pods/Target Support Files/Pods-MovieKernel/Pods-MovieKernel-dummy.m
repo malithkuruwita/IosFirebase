80505e79bbd0dac7bf4f0a97ed0064d7b0120cf5
malith <malithdulan7@gmail.com>
2019-04-24 00:11:12 -0700

    initialize the project ( adding pod dependencies and creating views, controllers )

---
+++ MovieKernel/Pods/Target Support Files/Pods-MovieKernel/Pods-MovieKernel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MovieKernel : NSObject
+@end
+@implementation PodsDummy_Pods_MovieKernel
+@end
---
+++ MovieKernel/Pods/Target Support Files/Pods-MovieKernel/Pods-MovieKernel-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_MovieKernelVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MovieKernelVersionString[];
+
